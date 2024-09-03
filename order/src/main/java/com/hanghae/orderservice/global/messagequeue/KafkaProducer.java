106c916d323a435ff083776a4cd9092de2470673
beunchoi <byungeun0228@gmail.com>
2024-09-04 00:16:16 +0900

    refactor : kafka producer 수정

--- order/src/main/java/com/hanghae/orderservice/global/messagequeue/KafkaProducer.java
+++ order/src/main/java/com/hanghae/orderservice/global/messagequeue/KafkaProducer.java
@@ -2,6 +2,7 @@ package com.hanghae.orderservice.global.messagequeue;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hanghae.orderservice.domain.order.dto.OrderResponseDto;
 import com.hanghae.orderservice.domain.order.dto.PaymentRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,18 +15,18 @@ import org.springframework.stereotype.Service;
 public class KafkaProducer {
   private final KafkaTemplate<String, String> kafkaTemplate;
 
-//  public void send(String topic, OrderResponseDto orderDto) {
-//    ObjectMapper mapper = new ObjectMapper();
-//    String jsonInString = "";
-//    try {
-//      jsonInString = mapper.writeValueAsString(orderDto);
-//    } catch (JsonProcessingException ex) {
-//      ex.printStackTrace();
-//    }
-//
-//    kafkaTemplate.send(topic, jsonInString);
-//    log.info("kafka producer 가 데이터를 보냈습니다. " + orderDto);
-//  }
+  public void sendToProduct(String topic, OrderResponseDto orderDto) {
+    ObjectMapper mapper = new ObjectMapper();
+    String jsonInString = "";
+    try {
+      jsonInString = mapper.writeValueAsString(orderDto);
+    } catch (JsonProcessingException ex) {
+      ex.printStackTrace();
+    }
+
+    kafkaTemplate.send(topic, jsonInString);
+    log.info("kafka producer 가 데이터를 보냈습니다. " + orderDto);
+  }
 
   public void send(String topic, PaymentRequest request) {
     ObjectMapper mapper = new ObjectMapper();
