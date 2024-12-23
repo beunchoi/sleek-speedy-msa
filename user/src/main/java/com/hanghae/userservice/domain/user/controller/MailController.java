5566704711abed49c86d6ca2acf65d4e5f8a939a
beunchoi <byungeun0228@gmail.com>
2024-12-23 23:50:21 +0900

    Refactor : 메일 컨트롤러 분리

---
+++ user/src/main/java/com/hanghae/userservice/domain/user/controller/MailController.java
@@ -0,0 +1,46 @@
+package com.hanghae.userservice.domain.user.controller;
+
+import com.hanghae.userservice.domain.user.dto.mail.MailAuthDto;
+import com.hanghae.userservice.domain.user.dto.mail.MailRequestDto;
+import com.hanghae.userservice.domain.user.service.MailService;
+import jakarta.mail.MessagingException;
+import jakarta.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user-service")
+public class MailController {
+
+  private final MailService mailService;
+
+  /* Send Email: 인증번호 전송 버튼 click */
+  @PostMapping("/users/sendEmail")
+  public Map<String, String> mailSend(@RequestBody @Valid MailRequestDto mailRequestDto)
+      throws MessagingException {
+    String code = mailService.sendSimpleMessage(mailRequestDto.getEmail());
+    // response를 JSON 문자열으로 반환
+    Map<String, String> response = new HashMap<>();
+    response.put("code", code);
+
+    return response;
+  }
+
+  /* Email Auth: 인증번호 입력 후 인증 버튼 click */
+  @PostMapping("/users/authEmail")
+  public String authCheck(@RequestBody @Valid MailAuthDto mailAuthDto) {
+    Boolean checked = mailService.checkAuthNum(mailAuthDto.getMail(), mailAuthDto.getAuthNum());
+    if (checked) {
+      return "이메일 인증 성공!";
+    }
+    else {
+      throw new IllegalArgumentException("이메일 인증 실패!");
+    }
+  }
+}
--- user/src/main/java/com/hanghae/userservice/domain/user/controller/UserController.java
+++ user/src/main/java/com/hanghae/userservice/domain/user/controller/UserController.java
@@ -1,19 +1,13 @@
 package com.hanghae.userservice.domain.user.controller;
 
-import com.hanghae.userservice.domain.user.dto.AddressRequestDto;
-import com.hanghae.userservice.domain.user.dto.AddressResponseDto;
-import com.hanghae.userservice.domain.user.dto.MailAuthDto;
-import com.hanghae.userservice.domain.user.dto.MailRequestDto;
 import com.hanghae.userservice.domain.user.dto.SignupRequestDto;
 import com.hanghae.userservice.domain.user.dto.SignupResponseDto;
 import com.hanghae.userservice.domain.user.dto.UserResponseDto;
-import com.hanghae.userservice.domain.user.service.MailService;
+import com.hanghae.userservice.domain.user.dto.address.AddressRequestDto;
+import com.hanghae.userservice.domain.user.dto.address.AddressResponseDto;
 import com.hanghae.userservice.domain.user.service.UserService;
-import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,32 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user-service")
 public class UserController {
 
-  private final MailService mailService;
   private final UserService userService;
-
-  /* Send Email: 인증번호 전송 버튼 click */
-  @PostMapping("/users/sendEmail")
-  public Map<String, String> mailSend(@RequestBody @Valid MailRequestDto mailRequestDto)
-      throws MessagingException {
-    String code = mailService.sendSimpleMessage(mailRequestDto.getEmail());
-    // response를 JSON 문자열으로 반환
-    Map<String, String> response = new HashMap<>();
-    response.put("code", code);
-
-    return response;
-  }
-
-  /* Email Auth: 인증번호 입력 후 인증 버튼 click */
-  @PostMapping("/users/authEmail")
-  public String authCheck(@RequestBody @Valid MailAuthDto mailAuthDto) {
-    Boolean checked = mailService.checkAuthNum(mailAuthDto.getMail(), mailAuthDto.getAuthNum());
-    if (checked) {
-      return "이메일 인증 성공!";
-    }
-    else {
-      throw new IllegalArgumentException("이메일 인증 실패!");
-    }
-  }
 
   @PostMapping("/users")
   public ResponseEntity<SignupResponseDto> signup(@Valid @RequestBody SignupRequestDto requestDto,
