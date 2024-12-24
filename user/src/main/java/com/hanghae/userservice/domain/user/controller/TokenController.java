fb2743cd0ac887185fa2890379bc9eb825ebb2b9
beunchoi <byungeun0228@gmail.com>
2024-12-24 17:36:07 +0900

    Refactor : 토큰 컨트롤러 수정

--- user/src/main/java/com/hanghae/userservice/domain/user/controller/TokenController.java
+++ user/src/main/java/com/hanghae/userservice/domain/user/controller/TokenController.java
@@ -22,20 +22,32 @@ public class TokenController {
   private final UserService userService;
 
   @PostMapping("/token")
-  public ResponseEntity<?> reissueToken(@CookieValue("Refresh-token") String refreshToken) {
-    String userId = jwtUtil.getUserInfoFromToken(refreshToken).getSubject();
+  public ResponseEntity<?> reissueToken(@CookieValue(value = "Refresh-token", required = false)
+  String refreshToken) {
 
+    // 리프레시 토큰 누락 여부 확인
+    if (refreshToken == null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("리프레시 토큰이 누락되었습니다.");
+    }
+
+    // 토큰 유효성 검증
     if (!jwtUtil.validateToken(refreshToken)) {
       return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("유효하지 않거나 만료된 토큰입니다.");
     }
 
+    String userId = jwtUtil.getUserInfoFromToken(refreshToken).getSubject();
+
+    // 저장된 리프레시 토큰과 상호 검증
     if (!tokenService.validateRefreshToken(userId, refreshToken)) {
       return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("토큰 정보가 일치하지 않습니다.");
     }
 
+    // 새로운 액세스 토큰 발급
     UserRoleEnum role = userService.getUserRole(userId);
-    String newAccessToken = JwtAuthenticationFilter.BEARER_PREFIX + jwtUtil.createAccessToken(userId, role);
+    String newAccessToken = JwtAuthenticationFilter
+        .BEARER_PREFIX + jwtUtil.createAccessToken(userId, role);
 
-    return ResponseEntity.status(HttpStatus.OK).header("Authorization", newAccessToken).build();
+    return ResponseEntity.status(HttpStatus.OK)
+        .header("Authorization", newAccessToken).body("새로운 액세스 토큰이 발급되었습니다.");
   }
 }
