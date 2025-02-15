12af08c991f6ea711ebd77ef435b516b45f804ce
beunchoi <byungeun0228@gmail.com>
2025-02-15 21:05:09 +0900

    feat : RedisCacheConfig 추가

---
+++ product/src/main/java/com/hanghae/productservice/domain/product/config/RedisCacheConfig.java
@@ -0,0 +1,33 @@
+package com.hanghae.productservice.domain.product.config;
+
+import java.time.Duration;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableCaching
+public class RedisCacheConfig {
+
+  @Bean
+  public CacheManager productCacheManager(RedisConnectionFactory redisConnectionFactory) {
+    RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+        .serializeKeysWith(RedisSerializationContext // Key - String 직렬화
+            .SerializationPair.fromSerializer(new StringRedisSerializer()))
+        .serializeValuesWith(RedisSerializationContext // Value - Json 직렬화
+            .SerializationPair.fromSerializer(new Jackson2JsonRedisSerializer<Object>(Object.class)))
+        .entryTtl(Duration.ofMinutes(1L));
+
+    return RedisCacheManager.RedisCacheManagerBuilder
+        .fromConnectionFactory(redisConnectionFactory)
+        .cacheDefaults(redisCacheConfiguration)
+        .build();
+  }
+}
