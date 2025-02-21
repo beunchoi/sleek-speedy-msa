9768d5df12357d0509913303027c32fcbb1563c7
beunchoi <byungeun0228@gmail.com>
2025-02-21 21:45:53 +0900

    feat : ProductEventProducer 추가

---
+++ product/src/main/java/com/hanghae/productservice/domain/product/producer/ProductEventProducer.java
@@ -0,0 +1,22 @@
+package com.hanghae.productservice.domain.product.producer;
+
+import com.hanghae.productservice.domain.product.config.RabbitMQConfig;
+import com.hanghae.productservice.domain.product.event.StockUpdateFailedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j(topic = "ProductEventProducer")
+public class ProductEventProducer {
+
+  private final RabbitTemplate rabbitTemplate;
+
+  public void publishFailedEvent(StockUpdateFailedEvent event) {
+    rabbitTemplate.convertAndSend(RabbitMQConfig.exchangeErr, RabbitMQConfig.queueErrPayment, event);
+    log.info("재고 업데이트 실패 이벤트 전송");
+  }
+
+}
