410ddd0c558265a6cc1dc7a43e45657f1984011a
beunchoi <byungeun0228@gmail.com>
2025-03-04 23:08:46 +0900

    refactor : 기본 생성자 추가

--- payment/src/main/java/com/hanghae/paymentservice/domain/payment/event/PaymentSuccessEvent.java
+++ payment/src/main/java/com/hanghae/paymentservice/domain/payment/event/PaymentSuccessEvent.java
@@ -2,9 +2,11 @@ package com.hanghae.paymentservice.domain.payment.event;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @Data
+@NoArgsConstructor
 public class PaymentSuccessEvent {
   private String productId;
   private String orderId;
--- product/src/main/java/com/hanghae/productservice/domain/product/event/PaymentSuccessEvent.java
+++ product/src/main/java/com/hanghae/productservice/domain/product/event/PaymentSuccessEvent.java
@@ -2,9 +2,11 @@ package com.hanghae.productservice.domain.product.event;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @Data
+@NoArgsConstructor
 public class PaymentSuccessEvent {
   private String productId;
   private String orderId;
